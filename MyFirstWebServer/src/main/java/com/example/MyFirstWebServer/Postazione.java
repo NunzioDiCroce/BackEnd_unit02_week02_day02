f97adb9add69fcc6af9e969183859ac7ee50d9c2
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-01 15:29:49 +0200

    Update Postazione.java

--- MyFirstWebServer/src/main/java/com/example/MyFirstWebServer/Postazione.java
+++ MyFirstWebServer/src/main/java/com/example/MyFirstWebServer/Postazione.java
@@ -1,5 +1,19 @@
 package com.example.MyFirstWebServer;
 
+import java.util.Set;
+
 public class Postazione {
 
+	private long id;
+
+	protected String descrizione;
+
+	@Enumerated(EnumType.STRING)
+	protected TipoPostazione tipoPostazione;
+
+	protected int numeroMassimoOccupanti;
+
+	private String citta;
+
+	protected Set<Prenotazione> prenotazioni;
 }
