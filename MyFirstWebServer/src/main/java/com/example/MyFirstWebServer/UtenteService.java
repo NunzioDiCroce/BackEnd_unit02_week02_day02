693c0232d808ee01c40a83c00b803343a1d0d6b1
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-01 15:52:04 +0200

    Update UtenteService.java

--- MyFirstWebServer/src/main/java/com/example/MyFirstWebServer/UtenteService.java
+++ MyFirstWebServer/src/main/java/com/example/MyFirstWebServer/UtenteService.java
@@ -2,6 +2,7 @@ package com.example.MyFirstWebServer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -12,15 +13,28 @@ public class UtenteService {
 	private List<Utente> utenti = new ArrayList<>();
 
 	// METODO PER SALVARE UN UTENTE IN ARRAYLIST
-	public Utente save(Utente utente) {
+	public Utente salva(Utente utente) {
 		this.utenti.add(utente);
 		return utente;
 
 	}
 
 	// METODO PER RECUPERARE GLI UTENTI
-	public List<Utente> getUtenti() {
+	public List<Utente> recuperaUtenti() {
 		return this.utenti;
 	}
+
+	// METODO PER RECUPERARE UN UTENTE SPECIFICO
+	public Optional<Utente> recuperaUtentePerId(long id) {
+		Utente utenteCercato = null;
+		for (Utente utente : utenti) {
+			if (utente.getId() == id) {
+				utenteCercato = utente;
+			}
+		}
+		return Optional.ofNullable(utenteCercato);
+	}
+
+	// METODO PER ELIMINARE UN UTENTE SPECIFICO
 
 }
