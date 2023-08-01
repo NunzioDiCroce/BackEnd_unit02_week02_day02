a22e60105ba3f3e9eefea0dfb61710053843cb54
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-01 15:28:07 +0200

    Update Prenotazione.java

--- MyFirstWebServer/src/main/java/com/example/MyFirstWebServer/Prenotazione.java
+++ MyFirstWebServer/src/main/java/com/example/MyFirstWebServer/Prenotazione.java
@@ -1,5 +1,29 @@
 package com.example.MyFirstWebServer;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+
 public class Prenotazione {
+
+	private long id;
+
+	protected Utente utente;
+
+	protected LocalDate dataPrenotazione;
+
+	protected Postazione postazione;
 
 }
