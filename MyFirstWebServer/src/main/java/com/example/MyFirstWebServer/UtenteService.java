8b9f06eccfed3d8d8bd99ed6b8f618883bb6a112
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-01 15:44:48 +0200

    Update UtenteService.java

--- MyFirstWebServer/src/main/java/com/example/MyFirstWebServer/UtenteService.java
+++ MyFirstWebServer/src/main/java/com/example/MyFirstWebServer/UtenteService.java
@@ -1,5 +1,26 @@
 package com.example.MyFirstWebServer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class UtenteService {
+
+	// DEFINIZIONE ARRAYLIST DI UTENTI
+	private List<Utente> utenti = new ArrayList<>();
+
+	// METODO PER SALVARE UN UTENTE IN ARRAYLIST
+	public Utente save(Utente utente) {
+		this.utenti.add(utente);
+		return utente;
+
+	}
+
+	// METODO PER RECUPERARE GLI UTENTI
+	public List<Utente> getUtenti() {
+		return this.utenti;
+	}
 
 }
